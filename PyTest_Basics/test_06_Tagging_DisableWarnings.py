3f2be39ef4b6587361ad0c5a1e5d5ea11386d8d1
“rajatt95” <rajatvermaa95@gmail.com>
2022-06-29 15:00:04 +0530

    UDEMY - TWI (API Testing using Python) (Completed - Section 12 (Skip Test Cases | Execute test cases conditionally))

---
+++ PyTest_Basics/test_06_Tagging_DisableWarnings.py
@@ -0,0 +1,39 @@
+# /**
+# * @author Rajat Verma
+# * https://www.linkedin.com/in/rajat-v-3b0685128/
+# * https://github.com/rajatt95
+# * https://rajatt95.github.io/
+# * https://github.com/stars/rajatt95/lists/udemy-testing-world-infotech
+# * https://github.com/rajatt95/Python_PyTest_TWI
+# *
+# * Course: Step by Step Rest API Testing using Python + Pytest +Allure (https://www.udemy.com/course/api-testing-python/)
+# * Tutor: Testing World Infotech (https://www.udemy.com/user/technology-world/)
+# */
+#
+# /***************************************************/
+
+import pytest
+
+# Approach #1 -> --disable-pytest-warnings
+# python3 -m pytest -v -s --disable-pytest-warnings -m "Smoke" PyTest_Basics
+
+# Approach #2 ->
+# Register the custom Marker (using Pytest.ini file)
+
+
+@pytest.mark.Regression
+@pytest.mark.Smoke
+@pytest.mark.UserDefined_TopPriority
+def test_registration_API():
+    print('This is test_logout_API......')
+
+
+@pytest.mark.Regression
+@pytest.mark.Sanity
+def test_login_API():
+    print('This is test_login_API......')
+
+
+@pytest.mark.Regression
+def test_logout_API():
+    print('This is test_logout_API......')
